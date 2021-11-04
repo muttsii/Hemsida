c8f1210ba8e1b2db90ce9c2c8bdd09ee240fa325
muttsii <mats.jonsson.linkoping@gmail.com>
2021-11-04 10:22:46 +0100

    fungerande formulär

--- automation.js
+++ automation.js
@@ -14,5 +14,40 @@ function carousel() {
   setTimeout(carousel, 3000);
 }
 
-// kontaktformulär
 
+function submitForm(){
+  console.log("debug");
+  //debugger; 
+
+  const förnamn = document.forms["guestbook"]["förnamn"].value;
+  const efternamn = document.forms["guestbook"]["efternamn"].value;
+  
+  console.log(förnamn);
+  console.log(efterepost);
+
+
+  //date
+
+  const date = Date.now();
+  console.log(date);
+  const today = new Date(date);
+  console.log(today);
+
+  //posts
+  var message = document.getElementById("message").innerHTML;
+
+  message +=`
+  ${förnamn}, (${efternamn})
+  <br>
+  ${today.toUTCString()}
+  <hr>
+  `;
+  document.getElementById("message").innerHTML = message;
+
+  return false;
+}
+
+// kontaktformulär
+ function kontakt(){
+   alert("Ditt meddelande är nu skickat...")
+ }
