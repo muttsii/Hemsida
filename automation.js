80555a02e99c1ab8fb2a3afed7925b8a4ff2e68a
muttsii <mats.jonsson.linkoping@gmail.com>
2021-11-09 20:32:41 +0100

    all skit får fan va klar nu

--- automation.js
+++ automation.js
@@ -4,7 +4,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("bildspel");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
@@ -12,39 +12,6 @@ function carousel() {
   if (myIndex > x.length) {myIndex = 1}
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 3000);
-}
-
-
-function submitForm(){
-  console.log("debug");
-  //debugger; 
-
-  const förnamn = document.forms["guestbook"]["förnamn"].value;
-  const efternamn = document.forms["guestbook"]["efternamn"].value;
-  
-  console.log(förnamn);
-  console.log(efterepost);
-
-
-  //date
-
-  const date = Date.now();
-  console.log(date);
-  const today = new Date(date);
-  console.log(today);
-
-  //posts
-  var message = document.getElementById("message").innerHTML;
-
-  message +=`
-  ${förnamn}, (${efternamn})
-  <br>
-  ${today.toUTCString()}
-  <hr>
-  `;
-  document.getElementById("message").innerHTML = message;
-
-  return false;
 }
 
 // kontaktformulär
